478cc9f3a6c2bb59381422f30ed946d8939b9b38
SkeltalFlamingo <99576301+SkeltalFlamingo@users.noreply.github.com>
2022-05-02 21:08:36 -0700

    added buttons

--- application/Main.java
+++ application/Main.java
@@ -2,96 +2,141 @@ package application;
 
 // javaFX imports
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class Main extends Application {
 
-	// mainMenu buttons
-	protected static Button registerProduct = new Button("Register New Product");
-	protected static Button registerCustomer = new Button("Register New Customer");
-	protected static Button registerDoctor = new Button("Register New Doctor");
-	protected static Button registerEmployee = new Button("Register New Employee");
-	protected static Button deletePerson = new Button("Delete a Person");
-	protected static Button orderProduct = new Button("order Product");
-	protected static Button deregisterProduct = new Button("deregister Product");
-	protected static Button addPrescription = new Button("add Perscription");
-	protected static Button deletePrescription = new Button("delete Prescription");
-	protected static Button registerSupplier = new Button("registerSupplier");
-	protected static Button deregisterSupplier = new Button("deregisterSupplier");
+    // home scene buttons
+    protected static Button Products = new Button("Products");
+    protected static Button Suppliers = new Button("Suppliers");
+    protected static Button Orders = new Button("Orders");
+    protected static Button People = new Button("People");
 
-	// home button
-	protected static Button goHome = new Button("Home");
+    // Product Buttons
+    protected static Button registerProduct = new Button("Register New Product");
+    protected static Button deregisterProduct = new Button("Deregister Product");
 
-	// window stuff
-	protected static Stage primaryStage;
-	protected static Scene home;
+    // Suppliers Buttons
+    protected static Button registerSupplier = new Button("RegisterSupplier");
+    protected static Button deregisterSupplier = new Button("DeregisterSupplier");
+    protected static Button registerOffering = new Button("Add Product to Supplier");
+    protected static Button deregisterOffering = new Button("Remove Product from Supplier");
 
-	// to be removed later
-	public static Scene empty;
+    // Orders Buttons
+    protected static Button orderProduct = new Button("Order Product");
+    protected static Button recieveOrder = new Button("Recieve Order");
+    
+    // People Buttons
+    protected static Button registerCustomer = new Button("Register New Customer");
+    protected static Button registerDoctor = new Button("Register New Doctor");
+    protected static Button registerEmployee = new Button("Register New Employee");
+    protected static Button deletePerson = new Button("Delete a Person");
+    protected static Button addPrescription = new Button("add Perscription");
+    protected static Button deletePrescription = new Button("delete Prescription");
 
-	// main
-	public static void main(String[] args) {
+    // home button
+    protected static Button goHome = new Button("Home");
 
-		// TODO
-		// read database to array lists
+    // window
+    protected static Stage primaryStage;
 
-		// launch the GUI
-		launch(args);
+    // scenes
+    protected static Scene home;
 
-		// TODO
-		// write changes to database
-	}
+    // layouts
+    protected static HBox homeMenu;
+    protected static VBox categories;
+    protected static VBox productsLay;
+    protected static VBox suppliersLay;
+    protected static VBox ordersLay;
+    protected static VBox peopleLay;
 
-	// Application process
-	@Override
-	public void start(Stage original) throws Exception {
-		primaryStage = original;
+    // to be removed later
+    public static Scene empty;
 
-		primaryStage.setTitle("Pharmacy Inventory Manager");
+    // main
+    public static void main(String[] args) {
+        // launch the GUI
+        launch(args);
+    }
 
-		// give main menu buttons event detection
-		MainMenuHandler watcher = new MainMenuHandler();
-		registerProduct.setOnAction(watcher);
-		registerCustomer.setOnAction(watcher);
-		registerDoctor.setOnAction(watcher);
-		registerEmployee.setOnAction(watcher);
-		deletePerson.setOnAction(watcher);
-		orderProduct.setOnAction(watcher);
-		deregisterProduct.setOnAction(watcher);
-		addPrescription.setOnAction(watcher);
-		deletePrescription.setOnAction(watcher);
-		registerSupplier.setOnAction(watcher);
-		deregisterSupplier.setOnAction(watcher);
+    // Application process
+    @Override
+    public void start(Stage original) throws Exception {
+        primaryStage = original;
 
-		// give general buttons event detection
-		goHome.setOnAction(watcher);
+        primaryStage.setTitle("Pharmacy Inventory Manager");
 
-		// create mainMenu and add Buttons
-		VBox mainMenu = new VBox();
-		Label mainLab = new Label("Main Menu");
-		mainMenu.getChildren().addAll(mainLab, registerProduct, registerCustomer, registerDoctor, registerEmployee,
-				deletePerson, orderProduct, deregisterProduct, addPrescription, deletePrescription, registerSupplier,
-				deregisterSupplier);
-		home = new Scene(mainMenu, 300, 300);
+        // give home menu buttons event detection
+        MainMenuHandler watcher = new MainMenuHandler();
+        Products.setOnAction(watcher);
+        Suppliers.setOnAction(watcher);
+        Orders.setOnAction(watcher);
+        People.setOnAction(watcher);
 
-		// create emptyMenu and add Button
-		VBox emptyMenu = new VBox();
-		Label unFinished = new Label("This option has not yet been implemented.");
-		emptyMenu.getChildren().addAll(unFinished, goHome);
-		empty = new Scene(emptyMenu, 300, 300);
+        // give sub menu buttons event detection
+        registerProduct.setOnAction(watcher);
+        registerCustomer.setOnAction(watcher);
+        registerDoctor.setOnAction(watcher);
+        registerEmployee.setOnAction(watcher);
+        deletePerson.setOnAction(watcher);
+        orderProduct.setOnAction(watcher);
+        deregisterProduct.setOnAction(watcher);
+        addPrescription.setOnAction(watcher);
+        deletePrescription.setOnAction(watcher);
+        registerSupplier.setOnAction(watcher);
+        deregisterSupplier.setOnAction(watcher);
+        recieveOrder.setOnAction(watcher);
+        registerOffering.setOnAction(watcher);
+        deregisterOffering.setOnAction(watcher);
 
-		// show main menu
-		primaryStage.setScene(home);
-		primaryStage.show();
-	}
+        // give general buttons event detection
+        goHome.setOnAction(watcher);
 
+        // create categories layout
+        categories = new VBox();
+        categories.getChildren().addAll(Products, Suppliers, Orders, People);
 
+        // set up Products layout
+        productsLay = new VBox();
+        Label productLab = new Label("Product Options:");
+        productsLay.getChildren().addAll(productLab, registerProduct, deregisterProduct, orderProduct);
+
+        // set up Suppliers layout
+        suppliersLay = new VBox();
+        Label suppliersLab = new Label("Supplier Options:");
+        suppliersLay.getChildren().addAll(suppliersLab, registerSupplier, deregisterSupplier, registerOffering, deregisterOffering);
+
+        // set up Orders layout
+        ordersLay = new VBox();
+        Label ordersLab = new Label("Order Options:");
+        ordersLay.getChildren().addAll(ordersLab, orderProduct, recieveOrder);
+
+        // set up People layout
+        peopleLay = new VBox();
+        Label peopleLab = new Label("People Options:");
+        peopleLay.getChildren().addAll(peopleLab, registerCustomer, registerDoctor, registerEmployee, deletePerson, addPrescription,
+                deletePrescription);
+
+        // set up homeMenu and add Buttons
+        homeMenu = new HBox();
+        homeMenu.getChildren().addAll(categories);
+        home = new Scene(homeMenu, 300, 300);
+
+        // create emptyMenu and add Button
+        VBox emptyMenu = new VBox();
+        Label unFinished = new Label("This option has not yet been implemented.");
+        emptyMenu.getChildren().addAll(unFinished, goHome);
+        empty = new Scene(emptyMenu, 300, 300);
+
+        // show main menu
+        primaryStage.setScene(home);
+        primaryStage.show();
+    }
 
 }
--- application/MainMenuHandler.java
+++ application/MainMenuHandler.java
@@ -1,59 +1,105 @@
 package application;
 
-
+//local imports
 import dBs.*;
+
 //javaFX imports
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.geometry.Orientation;
+import javafx.scene.control.Separator;
 
 public class MainMenuHandler implements EventHandler<ActionEvent> {
-	// handle
-	@Override
-	public void handle(ActionEvent event) {
 
-		if (event.getSource() == Main.registerProduct) {
-			Product.register(Main.primaryStage, Main.home);
+    public static void setupHome() {
 
-		} else if (event.getSource() == Main.registerCustomer) {
-			Customer.addCustomer(Main.primaryStage, Main.home);
+    }
 
-		} else if (event.getSource() == Main.registerDoctor) {
-			Doctor.addDoctor(Main.primaryStage, Main.home);
+    // handle
+    @Override
+    public void handle(ActionEvent event) {
 
-		} else if (event.getSource() == Main.registerEmployee) {
-			Employee.register(Main.primaryStage, Main.home);
+        // categories-menu events
+        if (event.getSource() == Main.Products) {
+            Main.homeMenu.getChildren().clear();
 
-		} else if (event.getSource() == Main.deletePerson) {
-			Person.removePerson(Main.primaryStage, Main.home);
+            final Separator sepHor = new Separator();
+            sepHor.setOrientation(Orientation.VERTICAL);
 
-		} else if (event.getSource() == Main.orderProduct) {
-			Main.primaryStage.setScene(Main.empty);
+            Main.homeMenu.getChildren().addAll(Main.categories, sepHor, Main.productsLay);
 
-		} else if (event.getSource() == Main.deregisterProduct) {
-			Product.deregister(Main.primaryStage, Main.home);
+        } else if (event.getSource() == Main.Suppliers) {
+            Main.homeMenu.getChildren().clear();
 
-		} else if (event.getSource() == Main.addPrescription) {
-			Prescription.addPerscription(Main.primaryStage, Main.home);
-			;
+            final Separator sepHor = new Separator();
+            sepHor.setOrientation(Orientation.VERTICAL);
 
-		} else if (event.getSource() == Main.deletePrescription) {
-			Main.primaryStage.setScene(Main.empty);
+            Main.homeMenu.getChildren().addAll(Main.categories, sepHor, Main.suppliersLay);
 
-		} else if (event.getSource() == Main.registerSupplier) {
-			Supplier.register(Main.primaryStage, Main.home);
+        } else if (event.getSource() == Main.Orders) {
+            Main.homeMenu.getChildren().clear();
 
-		} else if (event.getSource() == Main.deregisterSupplier) {
-			Supplier.deregisterSupplier(Main.primaryStage, Main.home);
+            final Separator sepHor = new Separator();
+            sepHor.setOrientation(Orientation.VERTICAL);
 
-		} else if (event.getSource() == Main.goHome) {
-			Main.primaryStage.setScene(Main.home);
-		}
-	}
+            Main.homeMenu.getChildren().addAll(Main.categories, sepHor, Main.ordersLay);
+
+        } else if (event.getSource() == Main.People) {
+            Main.homeMenu.getChildren().clear();
+
+            final Separator sepHor = new Separator();
+            sepHor.setOrientation(Orientation.VERTICAL);
+
+            Main.homeMenu.getChildren().addAll(Main.categories, sepHor, Main.peopleLay);
+
+        }
+
+        // sub-menu events
+        if (event.getSource() == Main.registerProduct) {
+            Product.register(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.registerCustomer) {
+            Customer.addCustomer(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.registerDoctor) {
+            Doctor.addDoctor(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.registerEmployee) {
+            Employee.register(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.deletePerson) {
+            Person.removePerson(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.orderProduct) {
+            Order.placeOrder(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.deregisterProduct) {
+            Product.deregister(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.addPrescription) {
+            Prescription.addPerscription(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.deletePrescription) {
+            Main.primaryStage.setScene(Main.empty);
+
+        } else if (event.getSource() == Main.registerSupplier) {
+            Supplier.register(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.deregisterSupplier) {
+            Supplier.deregisterSupplier(Main.primaryStage, Main.home);
+
+        } else if (event.getSource() == Main.goHome) {
+            Main.primaryStage.setScene(Main.home);
+
+        } else if (event.getSource() == Main.recieveOrder) {
+            Order.recieved(Main.primaryStage, Main.home);
+            
+        } else if (event.getSource() == Main.registerOffering) {
+            Offering.register(Main.primaryStage, Main.home);
+            
+        } else if (event.getSource() == Main.deregisterOffering) {
+            Offering.deregister(Main.primaryStage, Main.home);
+        }
+
+    }
 }
